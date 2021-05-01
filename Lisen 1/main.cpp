e084448e08f4782359bf14514cfbd14a1cd8fb74
Karine Grigoryan <karine.grigoryan@basic.local>
2021-05-01 20:40:32 +0400

    Lisen1

---
+++ Lisen 1/main.cpp
@@ -0,0 +1,16 @@
+
+using namespace std;
+
+int main() {
+
+int count;
+int age;
+cout<<"Enter a number ";
+cin>>count;
+cout<<"Enter a age ";
+cin>>age;
+cout<<"Count= "<<count<<endl;
+cout<<"Age= "<<age;
+
+return 0;
+}
